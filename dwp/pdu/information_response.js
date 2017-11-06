f1544b36bb9f1988b31ba04b4c19a29ae63c5582
Matheus Medeiros Sarmento <matheus.m.sarmento@gmail.com>
2017-11-06 20:40:33 -0200

    Removed validate function from new pdu

--- dwp/pdu/information_response.js
+++ dwp/pdu/information_response.js
@@ -9,29 +9,6 @@ const extend = require( 'util' )._extend
 
 var validate = function validate( object ) {
 
-   if ( data === undefined ) {
-      throw 'Object is undefined';
-   }
-
-   var err = { 'result': false, 'message': '' };
-
-   if ( data.executingSimulationInstances === undefined ) {
-      throw 'executingSimulationInstances undefined';
-   }
-
-   if ( data.executingSimulationInstances.id === undefined ) {
-      err.result = true;
-      err.message += 'executingSimulationInstances.id ';
-   }
-
-   if ( data.executingSimulationInstances.startTime === undefined ) {
-      err.result = true;
-      err.message += 'executingSimulationInstances.startTime ';
-   }
-
-   if ( err.result ) {
-      throw err.message + 'undefined';
-   }
 }
 
 var format = function format( data ) {
