dcafc12df87730d71899b10ea43b8114463d1bc3
Mikael Mello <mikaelmmello@gmail.com>
2018-05-30 12:22:30 -0300

    Add mocha/chai to unit tests and unit tests for factory.expose

---
+++ test/factory.js
@@ -0,0 +1,43 @@
+const { expect }  = require('chai');
+const factory = require('../dwp/factory');
+
+describe('Unit Tests factory.js', () => {
+  describe('expose packet', () => {
+    it('should throw with no begin tag', () => {
+      const errReason = 'begin tag was not found';
+      expect(() => factory.expose('test1/END/')).to.throw()
+        .with.property('reason', errReason);
+
+      expect(() => factory.expose('test2/END/')).to.throw()
+        .with.property('reason', errReason);
+
+      expect(() => factory.expose('test3/END/')).to.throw()
+        .with.property('reason', errReason);
+
+      expect(() => factory.expose('test4/END/')).to.throw()
+        .with.property('reason', errReason);
+    });
+
+    it('should throw with no end tag', () => {
+      const errReason = 'end tag was not found';
+      expect(() => factory.expose('/BEGIN/test1')).to.throw()
+        .with.property('reason', errReason);
+
+      expect(() => factory.expose('/BEGIN/test2')).to.throw()
+        .with.property('reason', errReason);
+
+      expect(() => factory.expose('/BEGIN/test3')).to.throw()
+        .with.property('reason', errReason);
+
+      expect(() => factory.expose('/BEGIN/test4')).to.throw()
+        .with.property('reason', errReason);
+    });
+
+    it('should return with no limit tags', () => {
+      expect(factory.expose('xx/BEGIN/test1/END/')).to.equal('test1');
+      expect(factory.expose('/BEGIN/test2/END/xx')).to.equal('test2');
+      expect(factory.expose('xxx/BEGIN/test3/END/xxx')).to.equal('test3');
+      expect(factory.expose('/BEGIN/test4/END/')).to.equal('test4');
+    });
+  });
+});
