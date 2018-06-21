42f2b696f234c3d1e6adb306cae290768565228a
Mikael Mello <mikaelmmello@gmail.com>
2018-06-21 20:52:55 -0300

    Add unit tests for common.js

---
+++ test/common.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const common = require('../dwp/common');
+
+describe('Unit Tests common.js', () => {
+  describe('flag ids', () => {
+    it('all flags have ids with different bits set', () => {
+      let current = 0;
+      Object.entries(common.Flags).forEach(([key, value]) => { // eslint-disable-line
+        expect(value & current).to.equal(0);
+        current = current | value; // eslint-disable-line
+      });
+    });
+  });
+
+  describe('worker state', () => {
+    it('executed and paused have different values', () => {
+      const state = common.WorkerState;
+      expect(state.EXECUTING).to.not.equal(state.PAUSED);
+    });
+  });
+
+  describe('terminated task code', () => {
+    it('terminated and denied have different values', () => {
+      const tCode = common.TerminateTaskCode;
+      expect(tCode.DENIED).to.not.equal(tCode.TERMINATED);
+    });
+  });
+});
